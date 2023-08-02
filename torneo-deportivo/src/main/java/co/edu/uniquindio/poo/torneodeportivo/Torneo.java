5f05206fd86ef40e919e644427d9ec3b1cc7ed77
cacandela <christiancandela@gmail.com>
2023-08-02 11:15:22 -0500

    Se adicionan pruebas y se completa el código del Torneo

--- torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Torneo.java
+++ torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Torneo.java
@@ -3,6 +3,19 @@ package co.edu.uniquindio.poo.torneodeportivo;
 import java.time.LocalDate;
 
 public record Torneo(String nombre, LocalDate fechaInicio, LocalDate fechaInicioInscripciones,
-        LocalDate fechaCierreInscripciones, byte numeroParticipantes, byte limiteEdad, int valorInscripcion) {
+        LocalDate fechaCierreInscripciones, Byte numeroParticipantes, Byte limiteEdad, Integer valorInscripcion) {
+
+    public Torneo {
+        assert nombre != null; 
+        assert numeroParticipantes == null || numeroParticipantes >= 0;
+        assert limiteEdad == null || limiteEdad >= 0;
+        assert valorInscripcion == null || valorInscripcion >= 0;
+        assert fechaInicio == null || fechaInicio.isAfter(fechaInicioInscripciones) && fechaInicio.isAfter(fechaCierreInscripciones);
+        assert fechaCierreInscripciones == null || fechaCierreInscripciones.isAfter(fechaInicioInscripciones);
+    }            
+
+    public Torneo(String nombre) {
+        this(nombre,null,null,null,null,null,null);
+    }
 
 }
--- torneo-deportivo/src/test/java/co/edu/uniquindio/poo/torneodeportivo/TorneoTest.java
+++ torneo-deportivo/src/test/java/co/edu/uniquindio/poo/torneodeportivo/TorneoTest.java
@@ -8,7 +8,7 @@
 package co.edu.uniquindio.poo.torneodeportivo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
 import java.util.logging.Logger;
@@ -19,37 +19,37 @@ public class TorneoTest {
     /**
      * Instacia para el menejo de logs
      */
-    private static final Logger log = Logger.getLogger(TorneoTest.class.getName());
+    private static final Logger LOG = Logger.getLogger(TorneoTest.class.getName());
     /**
      * Verificar que la clase Torneo almacene y recupere los datos 
      * 
      */
     @Test
     public void datosCompletos() {
-        log.info("Inicio de prueba datos completos...");
+        LOG.info("Inicio de prueba datos completos...");
         // Almacenar los datos de prueba Copa Mundo|2023-10-01|2023-08-01|2023-09-15|24|0|0
-        Torneo torneo = new Torneo("Copa Mundo", LocalDate.of(2023, 10, 1), LocalDate.of(2023, 8, 01), LocalDate.of(2023, 10, 15), (byte)0, (byte)0, 0);
+        Torneo torneo = new Torneo("Copa Mundo", LocalDate.of(2023, 10, 1), LocalDate.of(2023, 8, 1), LocalDate.of(2023, 9, 15), (byte)24, (byte)0, 0);
 
         // Recuperación y verificación de datos
         assertEquals("Copa Mundo",torneo.nombre());
         assertEquals(LocalDate.of(2023, 10, 1),torneo.fechaInicio());
         assertEquals(LocalDate.of(2023, 8, 1),torneo.fechaInicioInscripciones());
-        assertEquals(LocalDate.of(2023, 10, 15),torneo.fechaCierreInscripciones());
-        assertEquals(0,torneo.numeroParticipantes());
-        assertEquals(0,torneo.limiteEdad());
+        assertEquals(LocalDate.of(2023, 9, 15),torneo.fechaCierreInscripciones());
+        assertEquals((byte)24,torneo.numeroParticipantes());
+        assertEquals((byte)0,torneo.limiteEdad());
         assertEquals(0,torneo.valorInscripcion());
         
-        log.info("Fin de prueba datos completos...");
+        LOG.info("Fin de prueba datos completos...");
     }
 
     /**
-     * Verificar que la clase Torneo almacene y recupere los datos 
+     * Verificar que la clase Torneo permita almacenar solo el nombre 
      * 
      */
     @Test
     public void soloNombre() {
-        log.info("Inicio de prueba datos completos...");
-        // Almacenar los datos de prueba Copa Mundo|2023-10-01|2023-08-01|2023-09-15|24|0|0
+        LOG.info("Inicio de prueba solo nombre...");
+        // Almacenar los datos de prueba Copa Mundo
         Torneo torneo = new Torneo("Copa Mundo");
 
         // Recuperación y verificación de datos
@@ -61,7 +61,100 @@ public class TorneoTest {
         assertEquals(null,torneo.limiteEdad());
         assertEquals(null,torneo.valorInscripcion());
         
-        log.info("Fin de prueba datos completos...");
+        LOG.info("Fin de prueba solo nombre...");
     }
 
+    /**
+     * Verificar que la clase Torneo valide que se ingrese un nombre para el torneo 
+     * 
+     */
+    @Test
+    public void sinNombre() {
+        LOG.info("Inicio de prueba sin nombre...");
+        // Almacenar los datos de prueba null|2023-10-01|2023-08-01|2023-09-15|24|0|0
+        assertThrows(Throwable.class, ()-> new Torneo(null, LocalDate.of(2023, 10, 1), LocalDate.of(2023, 8, 01), LocalDate.of(2023, 10, 15), (byte)24, (byte)0, 0));
+        
+        // Almacenar los datos de prueba null
+        assertThrows(Throwable.class, ()-> new Torneo(null));
+
+        LOG.info("Fin de prueba sin nombre...");
+    }
+
+    /**
+     * Verificar que la clase Torneo valide que el ingreso de número de participantes negativo 
+     * 
+     */
+    @Test
+    public void participantesNegativos() {
+        LOG.info("Inicio de prueba número de participantes negativo...");
+        // Almacenar los datos de prueba Copa Mundo|2023-10-01|2023-08-01|2023-09-15|-24|0|0
+        assertThrows(Throwable.class, ()-> new Torneo("Copa Mundo", LocalDate.of(2023, 10, 1), LocalDate.of(2023, 8, 01), LocalDate.of(2023, 10, 15), (byte)-24, (byte)0, 0));
+        
+        LOG.info("Fin de prueba  número de participantes negativo...");
+    }
+
+    /**
+     * Verificar que la clase Torneo valide que el ingreso de limites de edades negativo 
+     * 
+     */
+    @Test
+    public void limiteEdadesNegativo() {
+        LOG.info("Inicio de prueba limites de edades negativo...");
+        // Almacenar los datos de prueba Copa Mundo|2023-10-01|2023-08-01|2023-09-15|24|-1|0
+        assertThrows(Throwable.class, ()-> new Torneo("Copa Mundo", LocalDate.of(2023, 10, 1), LocalDate.of(2023, 8, 01), LocalDate.of(2023, 10, 15), (byte)24, (byte)-1, 0));
+        
+        LOG.info("Fin de prueba  limites de edades negativo...");
+    }
+
+    /**
+     * Verificar que la clase Torneo valide que el ingreso de valor de inscripción negativa
+     * 
+     */
+    @Test
+    public void inscripcionNegativa() {
+        LOG.info("Inicio de prueba inscripción negativa...");
+        // Almacenar los datos de prueba Copa Mundo|2023-10-01|2023-08-01|2023-09-15|24|0|-1
+        assertThrows(Throwable.class, ()-> new Torneo("Copa Mundo", LocalDate.of(2023, 10, 1), LocalDate.of(2023, 8, 01), LocalDate.of(2023, 10, 15), (byte)24, (byte)0, -1));
+        
+        LOG.info("Fin de prueba inscripción negativa...");
+    }
+
+    /**
+     * Verificar que la clase Torneo valide que el ingreso de inscripciones posteriores a la fecha de inicio del torneo
+     * 
+     */
+    @Test
+    public void inscripcionTardia() {
+        LOG.info("Inicio de prueba inscripción tardia...");
+        // Almacenar los datos de prueba Copa Mundo|2023-10-01|2023-11-01|2023-11-15|24|0|0
+        assertThrows(Throwable.class, ()-> new Torneo("Copa Mundo", LocalDate.of(2023, 10, 1), LocalDate.of(2023, 11, 01), LocalDate.of(2023, 11, 15), (byte)24, (byte)0, 0));
+        
+        LOG.info("Fin de prueba inscripción tardia...");
+    }
+
+    /**
+     * Verificar que la clase Torneo valide que el ingreso de inicio inscripciones posteriores a la fecha de cierre de inscripciones
+     * 
+     */
+    @Test
+    public void cierreInscripcionAnteriorInicio() {
+        LOG.info("Inicio de prueba Cierre inscripción anterior al inicio...");
+        // Almacenar los datos de prueba Copa Mundo|2023-10-01|2023-11-01|2023-11-15|24|0|0
+        assertThrows(Throwable.class, ()-> new Torneo("Copa Mundo", LocalDate.of(2023, 10, 1), LocalDate.of(2023, 8, 15), LocalDate.of(2023, 8, 1), (byte)24, (byte)0, 0));
+        
+        LOG.info("Fin de prueba Cierre inscripción anterior al inicio...");
+    }
+
+        /**
+     * Verificar que la clase Torneo valide que el ingreso de inicio inscripciones posteriores a la fecha de cierre de inscripciones
+     * 
+     */
+    @Test
+    public void test() {
+        LOG.info("Inicio de prueba Cierre inscripción anterior al inicio...");
+        // Almacenar los datos de prueba Copa Mundo|2023-10-01|2023-11-01|2023-11-15|24|0|0
+        new Torneo("Copa Mundo", LocalDate.of(2023, 10, 1), null, LocalDate.of(2023, 8, 1), (byte)24, (byte)0, 0);
+        
+        LOG.info("Fin de prueba Cierre inscripción anterior al inicio...");
+    }
 }
