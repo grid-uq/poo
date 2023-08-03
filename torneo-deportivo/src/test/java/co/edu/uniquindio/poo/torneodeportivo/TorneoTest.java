7ef019e6a9c6b95e2c5dc4eb329e0ceccbaa291d
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2023-08-03 09:37:00 -0500

    Se modifica comentario

--- torneo-deportivo/src/test/java/co/edu/uniquindio/poo/torneodeportivo/TorneoTest.java
+++ torneo-deportivo/src/test/java/co/edu/uniquindio/poo/torneodeportivo/TorneoTest.java
@@ -11,7 +11,7 @@ import java.util.logging.Logger;
 
 public class TorneoTest {
     /**
-     * Instacia para el menejo de logs
+     * Instancia para el manejo de logs
      */
     private static final Logger LOG = Logger.getLogger(TorneoTest.class.getName());
     
