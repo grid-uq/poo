a6fac9848106c7a0f5a1040d402bf00c510c405e
cacandela <christiancandela@gmail.com>
2023-08-10 15:35:27 -0500

    Se ajusta el código de Torneo

--- torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Torneo.java
+++ torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Torneo.java
@@ -10,6 +10,6 @@ package co.edu.uniquindio.poo.torneodeportivo;
 import java.time.LocalDate;
 
 public record Torneo(String nombre, LocalDate fechaInicio, LocalDate fechaInicioInscripciones,
-        LocalDate fechaCierreInscripciones, Byte numeroParticipantes, Byte limiteEdad, Integer valorInscripcion) {
+        LocalDate fechaCierreInscripciones, byte numeroParticipantes, byte limiteEdad, int valorInscripcion) {
 
 }
