3d7ba5577f10de7e8384c2d684e67eecb39772ae
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2023-08-14 18:42:59 -0500

    Se adiciona prueba de asignación de null a fecha de inicio

--- torneo-deportivo/src/test/java/co/edu/uniquindio/poo/torneodeportivo/TorneoModificarFechasTest.java
+++ torneo-deportivo/src/test/java/co/edu/uniquindio/poo/torneodeportivo/TorneoModificarFechasTest.java
@@ -40,4 +40,20 @@ public class TorneoModificarFechasTest {
         LOG.info("Fin de prueba modificar fecha de inicio valida...");
     }
 
+    /**
+     * Verificar que la clase Torneo no permita la modificación de la fecha de inicio con un null
+     * 
+     */
+    @Test
+    public void modificarFechaInicioNull() {
+        LOG.info("Inicio de prueba modificar fecha de inicio null...");
+        // Almacenar los datos de prueba Copa Mundo|2023-10-01|2023-08-01|2023-09-15|24|0|0
+        Torneo torneo = new Torneo("Copa Mundo", LocalDate.of(2023, 10, 1), LocalDate.of(2023, 8, 1), LocalDate.of(2023, 9, 15), (byte)24, (byte)0, 0);
+
+        // Modificación de la fecha
+        assertThrows(Throwable.class,()->torneo.setFechaInicio(null));
+        
+        LOG.info("Fin de prueba modificar fecha de inicio null...");
+    }
+
 }
