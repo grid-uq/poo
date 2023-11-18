f94386fe03ea154674966cd1741bae99b4c0ce19
LauraMSanchez <131871346+LauraMSanchez@users.noreply.github.com>
2023-11-18 16:04:24 -0500

    Create Juez.java

---
+++ torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Juez.java
@@ -0,0 +1,27 @@
+package co.edu.uniquindio.poo.torneodeportivo;
+
+import static co.edu.uniquindio.poo.util.AssertionUtil.ASSERTION;
+
+public class Juez extends Persona{
+
+    private final String Licencia; 
+
+    public Juez (String nombre, String apellido, String email, String celular, String Licencia)
+    {
+        super(nombre, apellido, email, celular);
+
+        ASSERTION.assertion( Licencia != null && !Licencia.isBlank() , "La licencia es requerida");
+        assert Licencia.length() == 7 : "La licencia debe tener 7 dígitos.";
+        this.Licencia=Licencia;
+
+    }
+
+    
+    //GET Y SET
+    public String getLicencia() {
+        return Licencia;
+    }
+
+
+
+}
