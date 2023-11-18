8c31e3bd9cf632b329bd33b945d5418ea298aebf
lausanX <lauram.sanchezf@uqvirtual.edu.co>
2023-11-18 16:46:05 -0500

    Se crea la clase Representante

---
+++ torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Representante.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.poo.torneodeportivo;
+
+public class Representante extends Persona {
+
+public Representante(String nombre, String apellido, String email, String celular)
+{
+    super(nombre, apellido, email, celular);
+}    
+
+}
